5a845ca09d6e3733f4dd11e512783ad723d2d8f4
shasegwa <85041160+SHiren1995@users.noreply.github.com>
2022-12-15 20:36:42 +0900

    パスの差分を抽出するよう変更

--- main_script.py
+++ main_script.py
@@ -10,37 +10,49 @@ def audio_stft_spectrogram(input_data_path: str, output_data_path: str):
     plt.xlim(0, 60)
     plt.ylim(0, 1200)
 
-    # オーディオデータのパス取得
-    files: list = glob.glob(input_data_path + '*')
+    # 入力オーディオデータのパス取得
+    input_files: list = glob.glob(input_data_path + '*')
+    input_files_basename_without_ext :list = [ os.path.splitext(os.path.basename(file))[0] for file in input_files]
+
+    # 出力スペクトログラムのパス取得
+    output_files: list = glob.glob(output_data_path + '*')
+    output_files_basename_without_ext :list = [ os.path.splitext(os.path.basename(file))[0] for file in output_files]
+
+    # 入力ファイルと出力ファイルの差分取得
+    diff_files = set(input_files_basename_without_ext) - set(output_files_basename_without_ext)
+    input_file_path_list = [input_data_path + file for file in diff_files]
 
     # オーディオデータ → スペクトログラム
-    for file in files:
+    if len(input_file_path_list):
+        for file in input_file_path_list:
 
-        # オーディオファイル名・取得
-        basename_without_ext = os.path.splitext(os.path.basename(file))[0]
+            # オーディオファイル名・取得
+            basename_without_ext = os.path.splitext(os.path.basename(file))[0]
 
-        try:
-            # サンプリング周波数 22.05kHzで読み込み
-            y, sr = librosa.load(file)
+            try:
+                # サンプリング周波数 22.05kHzで読み込み
+                y, sr = librosa.load(file)
 
-            # 短時間フーリエ変換（STFT）
-            D = librosa.stft(y)
+                # 短時間フーリエ変換（STFT）
+                D = librosa.stft(y)
 
-            # 複素数を強度と位相へ変換
-            S, phase = librosa.magphase(D)
+                # 複素数を強度と位相へ変換
+                S, phase = librosa.magphase(D)
 
-            # 強度をdb単位へ変換
-            Sdb = librosa.amplitude_to_db(S)
+                # 強度をdb単位へ変換
+                Sdb = librosa.amplitude_to_db(S)
 
-            # スペクトログラムを書き込み
-            librosa.display.specshow(Sdb, sr=sr, x_axis='time', y_axis='log')
+                # スペクトログラムを書き込み
+                librosa.display.specshow(Sdb, sr=sr, x_axis='time', y_axis='log')
 
-            # スペクトログラム画像・生成
-            plt.savefig(output_data_path + basename_without_ext + '.png')
-            print(f'ファイル名：{basename_without_ext}.png スペクトログラム生成・完了')
+                # スペクトログラム画像・生成
+                plt.savefig(output_data_path + basename_without_ext + '.png')
+                print(f'ファイル名：{basename_without_ext}.png スペクトログラム生成・完了')
 
-        except:
-            print('スペクトログラム生成に失敗しました。')
+            except:
+                print('スペクトログラム生成に失敗しました。')
+    else:
+        print('出力可能な音声データが存在しません。')
 
 
 if __name__ == "__main__":
